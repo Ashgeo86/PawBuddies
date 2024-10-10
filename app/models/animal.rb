58be6e0dbf23dc640919517c1ef2f1fc3463a61d
Ashley George <ashgeo1986@gmail.com>
2024-10-10 14:57:35 +0100

    Ash added pics to seed

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,6 +1,6 @@
 class Animal < ApplicationRecord
   belongs_to :user
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   validates :name, :species, :price, :location, :description, presence: true
   validates :price, numericality: { greater_than: 0 }
   validates :description, length: { maximum: 200 }
